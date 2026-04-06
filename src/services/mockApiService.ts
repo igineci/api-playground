5c474fd6c29c355ff24f8e18984e6e4a7ef22ca2
Andjela Djekic <andjeladjek@gmail.com>
2026-04-06 14:21:53 +0200

    add ResponseDisplay component & utility formatting functions

--- src/services/mockApiService.ts
+++ src/services/mockApiService.ts
@@ -36,7 +36,6 @@ function buildMockBody(config: RequestConfig): unknown {
         data: [
           { id: 1, name: "Item 1" },
           { id: 2, name: "Item 2" },
-          { id: 3, name: "Item 3" },
         ],
       };
     case "POST":
